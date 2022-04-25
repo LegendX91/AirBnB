3fb51792aaa0466d40b62403c2ed99fbdf5cb725
LegendX91 <gabriele.papaleo@gmail.com>
2022-04-26 00:20:48 +0200

    first commit

---
+++ src/screens/Home/styles.js
@@ -0,0 +1,51 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 500,
+        resizeMode: 'cover',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 80,
+        fontWeight: 'bold',
+        color: 'white',
+        width: '70%',
+        marginLeft: 25
+    },
+    button: {
+        backgroundColor: '#fff',
+        width: 200,
+        marginLeft: 25,
+        height: 40,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 25
+    },
+    buttonText: {
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    searchButton: {
+        backgroundColor: '#fff',
+        width: Dimensions.get('screen').width - 20,
+        borderRadius: 30,
+        justifyContent: 'center',
+        marginHorizontal: 10,
+        alignItems: 'center',
+        flexDirection: 'row',
+        position: 'absolute',
+        top: 20,
+        height: 60,
+        elevation: 100, // android
+        zIndex: 100     // iOS
+    },
+    searchButtonText: {
+        fontSize: 16,
+        fontWeight: 'bold'
+    }
+});
+
+export default styles;
